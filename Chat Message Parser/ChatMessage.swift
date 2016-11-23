40dde39c0361a053ae54b16209688d112f238941
Phill Farrugia <me@phillfarrugia.com>
2016-11-23 17:03:48 +1100

    Applies parser property to each MessageDataType

--- Chat Message Parser/ChatMessage.swift
+++ Chat Message Parser/ChatMessage.swift
@@ -8,23 +8,6 @@
 
 import Foundation
 
-enum MessageDataType {
-    case Mentions
-    case Emoticons
-    case Links
-    
-    var title: String {
-        switch (self) {
-        case .Mentions:
-            return "mentions"
-        case .Emoticons:
-            return "emoticons"
-        case .Links:
-            return "links"
-        }
-    }
-}
-
 class ChatMessage {
     
     private let message: String
@@ -36,25 +19,14 @@ class ChatMessage {
     func parse(_ dataTypes: [MessageDataType]) -> [String: [AnyObject]] {
         var results: [String: [AnyObject]] = [:]
         for dataType in dataTypes {
-            switch (dataType) {
-            case .Mentions:
-                if let matches = resolve(parser: MessageParser(), for: message) {
-                    results[dataType.title] = matches
-                }
-            case .Emoticons:
-                if let matches = resolve(parser: EmoticonParser(), for: message) {
-                    results[dataType.title] = matches
-                }
-            case .Links:
-                if let matches = resolve(parser: LinkParser(), for: message) {
-                    results[dataType.title] = matches
-                }
+            if let matches = resolve(parser: dataType.parser, for: message) {
+                results[dataType.title] = matches
             }
         }
         return results
     }
     
-    func resolve<T: Parser>(parser: T, for string: String) -> [AnyObject]? {
+    func resolve(parser: Parser, for string: String) -> [AnyObject]? {
         let matches = parser.findMatches(in: string)
         if (matches.count > 0) {
             return matches
---
+++ Chat Message Parser/MessageDataType.swift
@@ -0,0 +1,37 @@
+//
+//  MessageDataType.swift
+//  Chat Message Parser
+//
+//  Created by Phill Farrugia on 23/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import Foundation
+
+enum MessageDataType {
+    case Mentions
+    case Emoticons
+    case Links
+    
+    var title: String {
+        switch (self) {
+        case .Mentions:
+            return "mentions"
+        case .Emoticons:
+            return "emoticons"
+        case .Links:
+            return "links"
+        }
+    }
+    
+    var parser: Parser {
+        switch (self) {
+        case .Mentions:
+            return MessageParser()
+        case .Emoticons:
+            return EmoticonParser()
+        case .Links:
+            return LinkParser()
+        }
+    }
+}
