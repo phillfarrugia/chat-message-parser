09e4dfb8a9bbd0d9b48347f12b4d4b1ebfbb4d67
Phill Farrugia <me@phillfarrugia.com>
2016-11-23 14:23:42 +1100

    Creates new Xcode Project

---
+++ Chat Message Parser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat Message Parser
+//
+//  Created by Phill Farrugia on 23/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Chat Message ParserTests/Chat_Message_ParserTests.swift
@@ -0,0 +1,36 @@
+//
+//  Chat_Message_ParserTests.swift
+//  Chat Message ParserTests
+//
+//  Created by Phill Farrugia on 23/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import XCTest
+@testable import Chat_Message_Parser
+
+class Chat_Message_ParserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
