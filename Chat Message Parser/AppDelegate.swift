73f427c222107ac792f931092bf4caf1a73d5c41
Phill Farrugia <me@phillfarrugia.com>
2016-11-23 16:54:59 +1100

    Adds sample message to delegate

--- Chat Message Parser/AppDelegate.swift
+++ Chat Message Parser/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let results = ChatMessage("@phill http://google.com.au/ (poop) (thisistoolongitwillnevergetmatched)").parse([.Mentions, .Links, .Emoticons])
+        print(results)
+        
         return true
     }
 
