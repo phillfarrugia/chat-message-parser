0509e4b4208d49ca5d4c1333899c81d6a4fed8c7
Phill Farrugia <me@phillfarrugia.com>
2016-11-23 20:53:35 +1100

    Implements tests for matches and values in string functions

--- Chat Message ParserTests/MentionParserSpec.swift
+++ Chat Message ParserTests/MentionParserSpec.swift
@@ -9,7 +9,11 @@
 import Quick
 import Nimble
 
-class MentionParserSpec: QuickSpec {
+class ParserSpec: QuickSpec {
+    
+    let invalidParser = MockInvalidParser()
+    let validParser = MockValidParser()
+    
     override func spec() {
         
         describe("matches in string") {
@@ -17,31 +21,48 @@ class MentionParserSpec: QuickSpec {
             context("valid regular expression", {
                 
                 context("input contains matches", {
-                    it("should return an array of matches", closure: {
-                        //
+                    it("should return an array containing a single match", closure: {
+                        expect(self.validParser.internalExpression).toNot(beNil())
+                        expect(self.validParser.matches(in: "&sample").count).to(equal(1))
+                    })
+                    
+                    it("should return an array containing multiple matches", closure: {
+                        expect(self.validParser.internalExpression).toNot(beNil())
+                        expect(self.validParser.matches(in: "&sample &sample").count).to(equal(2))
+                        expect(self.validParser.matches(in: "&sample &test &hello").count).to(equal(3))
+                        expect(self.validParser.matches(in: "&one &two &three &four").count).to(equal(4))
                     })
                 })
                 
                 context("input doesn't contains matches", {
                     it("should return an empty array", closure: {
-                        //
+                        expect(self.validParser.matches(in: "no matches here").count).to(equal(0))
+                        expect(self.validParser.matches(in: "").count).to(equal(0))
+                        expect(self.validParser.matches(in: "    ").count).to(equal(0))
                     })
                 })
                 
             })
             
             context("invalid regular expression", {
-                
-                it("should return an empty array", closure: { 
-                    //
+                it("should return an empty array", closure: {
+                    expect(self.invalidParser.internalExpression).to(beNil())
+                    expect(self.invalidParser.matches(in: "sample input string").count).to(equal(0))
+                    expect(self.invalidParser.matches(in: "@mention @name @sample").count).to(equal(0))
+                    expect(self.invalidParser.matches(in: "\thisIsAnInvalidRegexPattern/\\").count).to(equal(0))
                 })
-                
             })
             
         }
         
         describe("values in string for matches") {
-            //
+            
+            it("should map NSTextCheckingResults into correct string values", closure: {
+                let sampleString = "this is an &sample"
+                let matchResults = self.validParser.internalExpression?.matches(in: sampleString, range: NSMakeRange(0, sampleString.characters.count))
+                expect(self.validParser.values(in: sampleString, for: matchResults!)).to(equal(["&sample"]))
+            })
+            
         }
         
     }
