f80df5e92a514d75ec0922137fab667d77196697
Phill Farrugia <me@phillfarrugia.com>
2016-11-23 21:28:16 +1100

    Adds emoticonparserspec

---
+++ Chat Message ParserTests/EmoticonParserSpec.swift
@@ -0,0 +1,23 @@
+//
+//  Chat_Message_ParserTests.swift
+//  Chat Message ParserTests
+//
+//  Created by Phill Farrugia on 23/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class EmoticonParserSpec: QuickSpec {
+    
+    let emoticonParser = EmoticonParser()
+    
+    override func spec() {
+        
+        describe("matches in string") {
+            //
+        }
+        
+    }
+}
