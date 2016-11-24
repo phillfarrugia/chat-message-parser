c72b73d77857ded53c20aad4095d51020b60b40f
Phill Farrugia <me@phillfarrugia.com>
2016-11-24 11:42:46 +1100

    Implements title for links

--- Chat Message Parser/LinkParser.swift
+++ Chat Message Parser/LinkParser.swift
@@ -11,13 +11,33 @@ import Foundation
 struct LinkParser: Parser {
     
     internal var internalExpression: NSRegularExpression?
-    internal var pattern: String = "(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?"
+    internal var pattern: String = "(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w\\.-]*)*\\/?"
     
     init() {
         do { self.internalExpression = try NSRegularExpression(pattern: pattern, options: .caseInsensitive) }
         catch let error {
             print("\(error)")
         }
+    }
+    
+    func matches(in string: String) -> [AnyObject] {
+        guard let internalExpression = self.internalExpression else { return [] }
+        let matches = internalExpression.matches(in: string, range: NSMakeRange(0, string.characters.count))
+        let urls = values(in: string, for: matches)
+        return urls.map { ["url": $0, "title": title(for: $0)] } as [AnyObject]
+    }
+    
+    private func title(for urlString: String) -> String? {
+        if let url = URL(string: urlString) {
+            do {
+                let html = try NSString.init(contentsOf: url, encoding: String.Encoding.ascii.rawValue)
+                if let titles = HTMLParser(tag: "title").matches(in: html as String) as? [String] {
+                    return titles.first
+                }
+            }
+            catch let error { print("\(error)") }
+        }
+        return nil
     }
     
 }
