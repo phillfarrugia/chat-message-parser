d091bbf0aa2d87e5eab8ec15e52258bed8e6de63
Phill Farrugia <me@phillfarrugia.com>
2016-11-24 15:06:58 +1100

    Adds empty expectations for chat message spec

---
+++ Chat Message ParserTests/ChatMessageSpec.swift
@@ -0,0 +1,53 @@
+//
+//  Chat_Message_ParserTests.swift
+//  Chat Message ParserTests
+//
+//  Created by Phill Farrugia on 23/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class ChatMessageSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("parse data types", {
+            
+            it("should parse matches for a single data type", closure: {
+                
+            })
+            
+            it("should parse matches for multiple data types", closure: {
+                
+            })
+            
+        })
+        
+        describe("resolve parser for string", {
+            
+            it("should return matches from associated data type parser", closure: {
+                
+            })
+            
+            it("should return nil if no matches for data type", closure: {
+                
+            })
+            
+        })
+        
+        describe("serialize dictionary", {
+            
+            it("should serialize a dictionary into a valid json string", closure: {
+                
+            })
+            
+            it("should return an empty string if serialization fails", closure: {
+                
+            })
+            
+        })
+        
+    }
+}
